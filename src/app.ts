d38d6b29497f1f152067b13b7961cb583e407f07
Fabricio <fabriciobencomo.viloria@gmail.com>
2024-10-28 04:16:44 -0400

    secret token added to verify signature of webhook

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import express from 'express'
 import { envs } from './config/envs'
 import { GithubController } from './presentation/github/controller'
 import exp from 'constants'
+import { GithubSha256Middleware } from './presentation/middlewares/github-sha256.middleware'
 
 (() => {
   main()
@@ -13,6 +14,8 @@ function main() {
   const controller = new GithubController()
 
   app.use(express.json())
+
+  app.use(GithubSha256Middleware.verifyGithubSignature)
 
 
   app.post('/api/github', controller.webhookHandler)
